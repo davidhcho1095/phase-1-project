79b94a15674f6342df09f8754fa32cdc67ca7309
David Cho <davidhcho1095@gmail.com>
2022-07-14 11:19:05 -0400

    no more like button

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ dogbtn.addEventListener("click",()=>{
 getDog();
 });
 
-var post= document.getElementById("post");
+var post = document.getElementById("post");
 post.addEventListener("click", function(){
-    var commentBoxValue= document.getElementById("comment-box").value;
+    var commentBoxValue = document.getElementById("comment-box").value;
  
     var li = document.createElement("li");
     var text = document.createTextNode(commentBoxValue);
@@ -26,38 +26,3 @@ post.addEventListener("click", function(){
     document.getElementById("unordered").appendChild(li);
  
 });
-
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
-
-document.addEventListener("DOMContentLoaded", () => {
-
-    const hearts = document.querySelectorAll("span.like-glyph")
-    console.log(hearts)
-  
-    hearts.forEach(hearts => hearts.addEventListener("click", callB))
-  
-    function callB(hearts) {
-      console.log(hearts.target)
-      mimicServerCall()
-      .then( () => {
-        if (hearts.target.innerText === EMPTY_HEART) {
-          hearts.target.classList.add('activated-heart')
-          hearts.target.innerText = FULL_HEART
-        }
-        else {
-          hearts.target.classList.remove('activated-heart')
-            hearts.target.innerText = EMPTY_HEART
-        }
-      })
-      .catch(() => {
-        const errorMess = document.getElementById("modal")
-        console.log(errorMess)
-        errorMess.className = "show"
-  
-        setTimeout(() => {
-          const errorMess = document.getElementById("modal")
-          console.log(errorMess)
-          errorMess.className ="hidden"}, 3000)}
-      )}
-    })
