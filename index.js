15c16f049843ec8175a5e927b3abb2aaa3a447bb
David Cho <davidhcho1095@gmail.com>
2022-07-22 11:40:56 -0400

    changed var to const cause I'm a freaking idiot

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ dogbtn.addEventListener("mouseover",() => {
 getDog();
 });
 
-var post = document.getElementById("post");
+const post = document.getElementById("post");
 post.addEventListener("click", function() {
-    var commentBoxValue = document.getElementById("comment-box").value;
-    var li = document.createElement("li");
-    var text = document.createTextNode(commentBoxValue);
+    const commentBoxValue = document.getElementById("comment-box").value;
+    const li = document.createElement("li");
+    const text = document.createTextNode(commentBoxValue);
     li.appendChild(text);
     document.getElementById("unordered").appendChild(li);
 });
